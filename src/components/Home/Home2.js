8d67033f71c5ce209cd3d61584c361082c17b0af
Jul Hendri <meetredmist@gmail.com>
2021-11-24 09:49:33 +0700

    test

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/ssss.png";
+import myImg from "../../Assets/vecjull.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
